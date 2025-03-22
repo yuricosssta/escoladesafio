83041c6aab816c2f89a8d30f18db625eb19dbcf4
Yuri Costa <82777453+yuricosssta@users.noreply.github.com>
2025-03-22 11:04:07 +0000

    Adição inicial

--- FrontEnd/escola-desafio/app/providers.tsx
+++ FrontEnd/escola-desafio/app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import dynamic from "next/dynamic";
 
 const NextThemeProvider = dynamic(
--- FrontEnd/escola-desafio/src/config.ts
+++ FrontEnd/escola-desafio/src/config.ts
@@ -1,25 +1,14 @@
 const buildConfig = () => {
   const blogId = process.env.NEXT_PUBLIC_BLOG_ID;
   if (!blogId) throw new Error("NEXT_PUBLIC_BLOG_ID is missing");
-
   const name = process.env.NEXT_PUBLIC_BLOG_DISPLAY_NAME || "Travel.";
   const copyright = process.env.NEXT_PUBLIC_BLOG_COPYRIGHT || "Samantha";
   const defaultTitle =
     process.env.NEXT_DEFAULT_METADATA_DEFAULT_TITLE || "Travel with Samantha";
   const defaultDescription = process.env.NEXT_PUBLIC_BLOG_DESCRIPTION || "Blog about travel and lifestyle.";
 
-  // Nova configuração para API
-  const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001";
-
   return {
     baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
-    api: {
-      baseUrl: apiBaseUrl, // Adicionando a URL da API
-      endpoints: {
-        posts: `${apiBaseUrl}/posts`, // Endpoint para posts
-        users: `${apiBaseUrl}/post`, // Exemplo de outro endpoint
-      },
-    },
     blog: {
       name,
       copyright,
