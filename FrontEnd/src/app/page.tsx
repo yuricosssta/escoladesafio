d323075eef3c1a594bb4625f545acf2c32bdaac9
Yuri Costa <eng.yuricosta@gmail.com>
2025-11-01 22:04:16 -0300

    fix: expired token 2

--- FrontEnd/src/app/page.tsx
+++ FrontEnd/src/app/page.tsx
@@ -1,11 +1,14 @@
+import { Footer } from "@/components/Footer";
+import { Navbar } from "@/components/Navbar";
 import { PostList } from "@/components/PostList";
 
 export default function HomePage() {
   return (
-
     <div className="container mx-auto px-5 mb-10">
-          <PostList /> 
-        </div>
+      <Navbar />
+      <PostList />
+      <Footer />
+    </div>
 
     // <main className="p-8">
 
